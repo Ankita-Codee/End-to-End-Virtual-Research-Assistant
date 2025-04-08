452e2424eedcc7c663efd412fc8482458b4babee
Ankita-Codee <admane.ankita@gmail.com>
2025-04-08 14:08:19 -0700

    code files are ready with code skeleton

---
+++ agents.py
@@ -0,0 +1,10 @@
+import os
+from autogen import AssistantAgent
+from dotenv import load_dotenv # to secure the environment
+
+load_dotenv()
+
+class ResearchAgent():
+    pass
+
+   
---
+++ data_loader.py
@@ -0,0 +1,7 @@
+import requests # Request website and fetch data
+import xml.etree.ElementTree as ET #
+from scholarly import scholarly
+print("om")
+
+class DataLoader:
+    pass
